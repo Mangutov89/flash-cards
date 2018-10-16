681bd638efc834039b4206b73396a1c1ab744b55
leo mangutov and Mary McMurray <leo@>
2018-10-16 14:30:35 -0700

    add clickable to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,76 @@ $(document).ready(function() {
     $("#initially-showing").Toggle();
     $("#initially-hidden").Toggle();
   });
+  $(".clickable2").click(function() {
+    $("#initially-showing2").Toggle();
+    $("#initially-hidden2").Toggle();
+  });
+  $(".clickable3").click(function() {
+    $("#initially-showing3").Toggle();
+    $("#initially-hidden3").Toggle();
+  });
+  $(".clickable4").click(function() {
+    $("#initially-showing4").Toggle();
+    $("#initially-hidden4").Toggle();
+  });
+  $(".clickable5").click(function() {
+    $("#initially-showing5").Toggle();
+    $("#initially-hidden5").Toggle();
+  });
+  $(".clickable6").click(function() {
+    $("#initially-showing6").Toggle();
+    $("#initially-hidden6").Toggle();
+  });
+  $(".clickable7").click(function() {
+    $("#initially-showing7").Toggle();
+    $("#initially-hidden7").Toggle();
+  });
+  $(".clickable8").click(function() {
+    $("#initially-showing8").Toggle();
+    $("#initially-hidden8").Toggle();
+  });
+  $(".clickable9").click(function() {
+    $("#initially-showing9").Toggle();
+    $("#initially-hidden9").Toggle();
+  });
+  $(".clickable10").click(function() {
+    $("#initially-showing10").Toggle();
+    $("#initially-hidden10").Toggle();
+  });
+  $(".clickable11").click(function() {
+    $("#initially-showing11").Toggle();
+    $("#initially-hidden11").Toggle();
+  });
+  $(".clickable12").click(function() {
+    $("#initially-showing12").Toggle();
+    $("#initially-hidden12").Toggle();
+  });
+  $(".clickable13").click(function() {
+    $("#initially-showing13").Toggle();
+    $("#initially-hidden13").Toggle();
+  });
+  $(".clickable14").click(function() {
+    $("#initially-showing14").Toggle();
+    $("#initially-hidden14").Toggle();
+  });
+  $(".clickable15").click(function() {
+    $("#initially-showing15").Toggle();
+    $("#initially-hidden15").Toggle();
+  });
+  $(".clickable16").click(function() {
+    $("#initially-showing16").Toggle();
+    $("#initially-hidden16").Toggle();
+  });
+  $(".clickable17").click(function() {
+    $("#initially-showing17").Toggle();
+    $("#initially-hidden17").Toggle();
+  });
+  $(".clickable18").click(function() {
+    $("#initially-showing18").Toggle();
+    $("#initially-hidden18").Toggle();
+  });
+  $(".clickable19").click(function() {
+    $("#initially-showing19").Toggle();
+    $("#initially-hidden19").Toggle();
+  });
 });
