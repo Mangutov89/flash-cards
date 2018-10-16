14feca9ecfd04a412668bac3988eab1a9bc3f68c
leo mangutov and Mary McMurray <leo@>
2018-10-16 16:46:20 -0700

    add shorter jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,83 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#initially-showing").toggle();
-    $("#initially-hidden").toggle();
-  });
-  $(".clickable2").click(function() {
-    $("#initially-showing2").toggle();
-    $("#initially-hidden2").toggle();
-  });
-  $(".clickable3").click(function() {
-    $("#initially-showing3").toggle();
-    $("#initially-hidden3").toggle();
-  });
-  $(".clickable4").click(function() {
-    $("#initially-showing4").toggle();
-    $("#initially-hidden4").toggle();
-  });
-  $(".clickable5").click(function() {
-    $("#initially-showing5").toggle();
-    $("#initially-hidden5").toggle();
-  });
-  $(".clickable6").click(function() {
-    $("#initially-showing6").toggle();
-    $("#initially-hidden6").toggle();
-  });
-  $(".clickable7").click(function() {
-    $("#initially-showing7").toggle();
-    $("#initially-hidden7").toggle();
-  });
-  $(".clickable8").click(function() {
-    $("#initially-showing8").toggle();
-    $("#initially-hidden8").toggle();
-  });
-  $(".clickable9").click(function() {
-    $("#initially-showing9").toggle();
-    $("#initially-hidden9").toggle();
-  });
-  $(".clickable10").click(function() {
-    $("#initially-showing10").toggle();
-    $("#initially-hidden10").toggle();
-  });
-  $(".clickable11").click(function() {
-    $("#initially-showing11").toggle();
-    $("#initially-hidden11").toggle();
-  });
-  $(".clickable12").click(function() {
-    $("#initially-showing12").toggle();
-    $("#initially-hidden12").toggle();
-  });
-  $(".clickable13").click(function() {
-    $("#initially-showing13").toggle();
-    $("#initially-hidden13").toggle();
-  });
-  $(".clickable14").click(function() {
-    $("#initially-showing14").toggle();
-    $("#initially-hidden14").toggle();
-  });
-  $(".clickable15").click(function() {
-    $("#initially-showing15").toggle();
-    $("#initially-hidden15").toggle();
-  });
-  $(".clickable16").click(function() {
-    $("#initially-showing16").toggle();
-    $("#initially-hidden16").toggle();
-  });
-  $(".clickable17").click(function() {
-    $("#initially-showing17").toggle();
-    $("#initially-hidden17").toggle();
-  });
-  $(".clickable18").click(function() {
-    $("#initially-showing18").toggle();
-    $("#initially-hidden18").toggle();
-  });
-  $(".clickable19").click(function() {
-    $("#initially-showing19").toggle();
-    $("#initially-hidden19").toggle();
+//   $(".clickable").click(function() {
+//     $("#initially-showing").toggle();
+//     $("#initially-hidden").toggle();
+//   });
+//   $(".clickable2").click(function() {
+//     $("#initially-showing2").toggle();
+//     $("#initially-hidden2").toggle();
+//   });
+//   $(".clickable3").click(function() {
+//     $("#initially-showing3").toggle();
+//     $("#initially-hidden3").toggle();
+//   });
+//   $(".clickable4").click(function() {
+//     $("#initially-showing4").toggle();
+//     $("#initially-hidden4").toggle();
+//   });
+//   $(".clickable5").click(function() {
+//     $("#initially-showing5").toggle();
+//     $("#initially-hidden5").toggle();
+//   });
+//   $(".clickable6").click(function() {
+//     $("#initially-showing6").toggle();
+//     $("#initially-hidden6").toggle();
+//   });
+//   $(".clickable7").click(function() {
+//     $("#initially-showing7").toggle();
+//     $("#initially-hidden7").toggle();
+//   });
+//   $(".clickable8").click(function() {
+//     $("#initially-showing8").toggle();
+//     $("#initially-hidden8").toggle();
+//   });
+//   $(".clickable9").click(function() {
+//     $("#initially-showing9").toggle();
+//     $("#initially-hidden9").toggle();
+//   });
+//   $(".clickable10").click(function() {
+//     $("#initially-showing10").toggle();
+//     $("#initially-hidden10").toggle();
+//   });
+//   $(".clickable11").click(function() {
+//     $("#initially-showing11").toggle();
+//     $("#initially-hidden11").toggle();
+//   });
+//   $(".clickable12").click(function() {
+//     $("#initially-showing12").toggle();
+//     $("#initially-hidden12").toggle();
+//   });
+//   $(".clickable13").click(function() {
+//     $("#initially-showing13").toggle();
+//     $("#initially-hidden13").toggle();
+//   });
+//   $(".clickable14").click(function() {
+//     $("#initially-showing14").toggle();
+//     $("#initially-hidden14").toggle();
+//   });
+//   $(".clickable15").click(function() {
+//     $("#initially-showing15").toggle();
+//     $("#initially-hidden15").toggle();
+//   });
+//   $(".clickable16").click(function() {
+//     $("#initially-showing16").toggle();
+//     $("#initially-hidden16").toggle();
+//   });
+//   $(".clickable17").click(function() {
+//     $("#initially-showing17").toggle();
+//     $("#initially-hidden17").toggle();
+//   });
+//   $(".clickable18").click(function() {
+//     $("#initially-showing18").toggle();
+//     $("#initially-hidden18").toggle();
+//   });
+//   $(".clickable19").click(function() {
+//     $("#initially-showing19").toggle();
+//     $("#initially-hidden19").toggle();
+//   });
+// });
+  $(".well").click(function() {
+    $(this).children(".hidecard").toggle();
+    $(this).children(".showcard").toggle();
   });
 });
