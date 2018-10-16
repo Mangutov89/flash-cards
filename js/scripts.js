96d522ae3ca150385bd500ae0dcc9096f313ee94
leo mangutov and Mary McMurray <leo@>
2018-10-16 14:51:47 -0700

    add working toggles

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,78 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").fadeToggle();
-    $("#initially-hidden").fadeToggle();
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
   $(".clickable2").click(function() {
-    $("#initially-showing2").slideToggle();
-    $("#initially-hidden2").slideToggle();
+    $("#initially-showing2").toggle();
+    $("#initially-hidden2").toggle();
   });
   $(".clickable3").click(function() {
-    $("#initially-showing3").slideToggle();
-    $("#initially-hidden3").slideToggle();
+    $("#initially-showing3").toggle();
+    $("#initially-hidden3").toggle();
   });
   $(".clickable4").click(function() {
-    $("#initially-showing4").Toggle();
-    $("#initially-hidden4").Toggle();
+    $("#initially-showing4").toggle();
+    $("#initially-hidden4").toggle();
   });
   $(".clickable5").click(function() {
-    $("#initially-showing5").Toggle();
-    $("#initially-hidden5").Toggle();
+    $("#initially-showing5").toggle();
+    $("#initially-hidden5").toggle();
   });
   $(".clickable6").click(function() {
-    $("#initially-showing6").Toggle();
-    $("#initially-hidden6").Toggle();
+    $("#initially-showing6").toggle();
+    $("#initially-hidden6").toggle();
   });
   $(".clickable7").click(function() {
-    $("#initially-showing7").Toggle();
-    $("#initially-hidden7").Toggle();
+    $("#initially-showing7").toggle();
+    $("#initially-hidden7").toggle();
   });
   $(".clickable8").click(function() {
-    $("#initially-showing8").Toggle();
-    $("#initially-hidden8").Toggle();
+    $("#initially-showing8").toggle();
+    $("#initially-hidden8").toggle();
   });
   $(".clickable9").click(function() {
-    $("#initially-showing9").Toggle();
-    $("#initially-hidden9").Toggle();
+    $("#initially-showing9").toggle();
+    $("#initially-hidden9").toggle();
   });
   $(".clickable10").click(function() {
-    $("#initially-showing10").Toggle();
-    $("#initially-hidden10").Toggle();
+    $("#initially-showing10").toggle();
+    $("#initially-hidden10").toggle();
   });
   $(".clickable11").click(function() {
-    $("#initially-showing11").Toggle();
-    $("#initially-hidden11").Toggle();
+    $("#initially-showing11").toggle();
+    $("#initially-hidden11").toggle();
   });
   $(".clickable12").click(function() {
-    $("#initially-showing12").Toggle();
-    $("#initially-hidden12").Toggle();
+    $("#initially-showing12").toggle();
+    $("#initially-hidden12").toggle();
   });
   $(".clickable13").click(function() {
-    $("#initially-showing13").Toggle();
-    $("#initially-hidden13").Toggle();
+    $("#initially-showing13").toggle();
+    $("#initially-hidden13").toggle();
   });
   $(".clickable14").click(function() {
-    $("#initially-showing14").Toggle();
-    $("#initially-hidden14").Toggle();
+    $("#initially-showing14").toggle();
+    $("#initially-hidden14").toggle();
   });
   $(".clickable15").click(function() {
-    $("#initially-showing15").Toggle();
-    $("#initially-hidden15").Toggle();
+    $("#initially-showing15").toggle();
+    $("#initially-hidden15").toggle();
   });
   $(".clickable16").click(function() {
-    $("#initially-showing16").Toggle();
-    $("#initially-hidden16").Toggle();
+    $("#initially-showing16").toggle();
+    $("#initially-hidden16").toggle();
   });
   $(".clickable17").click(function() {
-    $("#initially-showing17").Toggle();
-    $("#initially-hidden17").Toggle();
+    $("#initially-showing17").toggle();
+    $("#initially-hidden17").toggle();
   });
   $(".clickable18").click(function() {
-    $("#initially-showing18").Toggle();
-    $("#initially-hidden18").Toggle();
+    $("#initially-showing18").toggle();
+    $("#initially-hidden18").toggle();
   });
   $(".clickable19").click(function() {
-    $("#initially-showing19").Toggle();
-    $("#initially-hidden19").Toggle();
+    $("#initially-showing19").toggle();
+    $("#initially-hidden19").toggle();
   });
 });
